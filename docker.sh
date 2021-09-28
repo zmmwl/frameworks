89d7ee1a95321c39ba3202020057cd995fcd2b86
root <root@localhost.localdomain>
2021-09-28 08:37:47 -0400

    test

---
+++ docker.sh
@@ -0,0 +1,1 @@
+docker build -t ubuntu:16.04z .
--- mp-spdz/install.sh
+++ mp-spdz/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-curl -L https://github.com/data61/MP-SPDZ/releases/download/v0.1.5/mp-spdz-0.1.5.tar.xz | tar xJv
+tar xJv -f /root/mp-spdz-0.1.5.tar.xz 
 mv mp-spdz-0.1.5 MP-SPDZ
 
 cd MP-SPDZ
